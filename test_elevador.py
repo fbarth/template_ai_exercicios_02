f6ec42623c0ce791c319c0f7871dc260eca07a1f
Fabricio Barth <fabricio.barth@gmail.com>
2025-02-14 18:28:58 -0300

    atualizando testes

--- test_elevador.py
+++ test_elevador.py
@@ -30,3 +30,18 @@ def test_10_bp(capsys):
     main(0,10,'BP')
     captured = capsys.readouterr()
     assert captured.out == 'Solução:  ; subir 1 andar ; subir 1 andar ; subir 1 andar ; subir 1 andar ; subir 1 andar ; subir 1 andar ; subir 1 andar ; subir 1 andar ; subir 1 andar ; subir 1 andar\n'
+
+def test_2_bpi(capsys):
+    main(0,2,'BPI')
+    captured = capsys.readouterr()
+    assert captured.out == 'Solução:  ; subir 2 andares\n'
+
+def test_3_bpi(capsys):
+    main(0,3,'BPI')
+    captured = capsys.readouterr()
+    assert captured.out == 'Solução:  ; subir 1 andar ; subir 2 andares\n'
+
+def test_10_bpi(capsys):
+    main(0,10,'BPI')
+    captured = capsys.readouterr()
+    assert captured.out == 'Solução:  ; subir 2 andares ; subir 2 andares ; subir 2 andares ; subir 2 andares ; subir 2 andares\n'
